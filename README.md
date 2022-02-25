d5ee70ecb53fbe8891e043ca8cf60a5b37f599c3
ActiveX43 <yunks2000@naver.com>
2022-02-26 04:45:55 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Translation Automation Tool at Excel
+
+## How to use
+1 Subscribe Papago API in Naver Developer Tool
+2 Write Down "config.txt"
+3 Run Python File
+
+
+## Introduction to "config.txt"
+* FILE_NAME: Write Down **SINGLE** Excel File Name You Want To Translate.
+             Don't Omit ".xlsx" at the end.
+* SHEET_NAME: Write Down Excel Sheet Names You Want To Translate.
+              Multiple Sheets Are Available. "," Is the Delimiter.
+* INDEX_BEFORE:
---
+++ papago_excel.py
@@ -0,0 +1,93 @@
+import os
+import sys
+import urllib.request
+import pandas as pd
+
+def init_request(client_id, client_secret):
+    url = "https://openapi.naver.com/v1/papago/n2mt"
+    request = urllib.request.Request(url)
+    request.add_header("X-Naver-Client-Id", client_id)
+    request.add_header("X-Naver-Client-Secret", client_secret)
+    return request
+
+def get_papago(request, source, source_lang, target_lang):
+    encText = urllib.parse.quote(source)
+    data = f"source={source_lang}&target={target_lang}&text=" + encText
+    try:
+        with urllib.request.urlopen(request, data=data.encode("utf-8")) as response:
+            rescode = response.getcode()
+            if (rescode == 200):
+                response_body = response.read()
+                return response_body.decode('utf-8')
+            else:
+                print("Error Code:" + rescode)
+                return None
+    except urllib.request.HTTPError as e:
+        print(e)
+        return None
+
+config = {}
+with open("config.txt", "r", encoding = "utf-8") as f:
+    for line in f.readlines():
+        line = line.rstrip()
+        key, data = line.split(':')
+        if key == "SHEET_NAME":
+            config[key] = data.split(',')
+        else:
+            config[key] = data
+
+try:
+    config_required = [
+                "FILE_NAME",
+                "SHEET_NAME",
+                "INDEX_BEFORE",
+                "INDEX_AFTER",
+                "CLIENT_ID",
+                "CLIENT_SECRET",
+                "SOURCE_LANG",
+                "TARGET_LANG"
+            ]
+    for c in config_required:
+        config[c]
+except KeyError:
+    print("Error: No Required Configuration Value")
+    exit()
+
+if not config['FILE_NAME'].endswith(".xlsx"):
+    print("Error: inappropriate excel file name")
+    exit()
+
+request = init_request(config['CLIENT_ID'], config['CLIENT_SECRET'])
+
+try:
+    with pd.ExcelFile(config['FILE_NAME']) as reader:
+        for s in config["SHEET_NAME"]:
+            try:
+                xlsx = pd.read_excel(reader, sheet_name=s)
+                if config['INDEX_BEFORE'] not in xlsx.columns:
+                    print("Error: INDEX_BEFORE value not in file")
+                    exit()
+                elif config['INDEX_AFTER'] not in xlsx.columns:
+                    print("Error: INDEX_AFTER value not in file")
+                    exit()
+                else:
+                    for i in xlsx.index:
+                        data_before = xlsx.loc[i, config['INDEX_BEFORE']]
+                        response = get_papago(request, data_before, 
+                                config["SOURCE_LANG"], config['TARGET_LANG'])
+                        if response is None:
+                            print("Operation Interrupted due to Server Error")
+                            xlsx.to_excel(f"{config['FILE_NAME'][:-5]}_translated.xlsx")
+                            exit()
+                        else:
+                            data_after = response['message']['result']['translatedText']
+                            xlsx.loc[i, config['INDEX_AFTER']] = data_after
+            except ValueError:
+                print("Error: No such sheet")
+                exit()
+
+        xlsx.to_excel(f"{config['FILE_NAME'][:-5]}_translated.xlsx")
+
+except FileNotFoundError:
+    print("Error: No such file")
+    exit()
